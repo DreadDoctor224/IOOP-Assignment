66c64425c99d0812c9fb2738986e6baeb346a53c
jeff liew <liewjeff14@gmail.com>
2024-03-02 20:04:20 +0800

    Added a text box

--- IOOP Assignment/Form1.Designer.cs
+++ IOOP Assignment/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace IOOP_Assignment
+﻿using System;
+
+namespace IOOP_Assignment
 {
     partial class Form1
     {
@@ -29,6 +31,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -36,16 +39,24 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(600, 134);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "By Elvis";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,9 +66,15 @@
 
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
