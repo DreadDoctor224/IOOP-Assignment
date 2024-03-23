4114397f44b0b7bb5e378e494507dd89ba5a37fd
megadata-kaung <kaung.hkmo@gmail.com>
2024-03-23 17:52:33 +0800

    name changes payment view from coach

--- IOOP Assignment/Resources/View Payment Coach.Designer.cs
+++ IOOP Assignment/Resources/View Payment Coach.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.btn_hmpage001 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn_hmpg3 = new System.Windows.Forms.Button();
+            this.Btn_exit3 = new System.Windows.Forms.Button();
+            this.Lblpaymentcoach = new System.Windows.Forms.Label();
+            this.DataGridView3 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_hmpage001
@@ -53,56 +53,56 @@
             this.btn_hmpage001.Text = "Home Page";
             this.btn_hmpage001.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // Btn_hmpg3
             // 
-            this.button1.BackColor = System.Drawing.Color.Teal;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Franklin Gothic Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(12, 583);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 73);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "Home Page";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Btn_hmpg3.BackColor = System.Drawing.Color.Teal;
+            this.Btn_hmpg3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_hmpg3.Font = new System.Drawing.Font("Franklin Gothic Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_hmpg3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Btn_hmpg3.Location = new System.Drawing.Point(12, 583);
+            this.Btn_hmpg3.Name = "Btn_hmpg3";
+            this.Btn_hmpg3.Size = new System.Drawing.Size(132, 73);
+            this.Btn_hmpg3.TabIndex = 24;
+            this.Btn_hmpg3.Text = "Home Page";
+            this.Btn_hmpg3.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // Btn_exit3
             // 
-            this.button2.BackColor = System.Drawing.Color.Teal;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Franklin Gothic Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(529, 594);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 73);
-            this.button2.TabIndex = 23;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = false;
+            this.Btn_exit3.BackColor = System.Drawing.Color.Teal;
+            this.Btn_exit3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_exit3.Font = new System.Drawing.Font("Franklin Gothic Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_exit3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Btn_exit3.Location = new System.Drawing.Point(529, 594);
+            this.Btn_exit3.Name = "Btn_exit3";
+            this.Btn_exit3.Size = new System.Drawing.Size(143, 73);
+            this.Btn_exit3.TabIndex = 23;
+            this.Btn_exit3.Text = "Exit";
+            this.Btn_exit3.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // Lblpaymentcoach
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 32);
-            this.label1.TabIndex = 25;
-            this.label1.Text = "Payment";
+            this.Lblpaymentcoach.AutoSize = true;
+            this.Lblpaymentcoach.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblpaymentcoach.Location = new System.Drawing.Point(12, 22);
+            this.Lblpaymentcoach.Name = "Lblpaymentcoach";
+            this.Lblpaymentcoach.Size = new System.Drawing.Size(134, 32);
+            this.Lblpaymentcoach.TabIndex = 25;
+            this.Lblpaymentcoach.Text = "Payment";
             // 
-            // dataGridView1
+            // DataGridView3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3,
             this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(18, 83);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(654, 456);
-            this.dataGridView1.TabIndex = 26;
+            this.DataGridView3.Location = new System.Drawing.Point(18, 83);
+            this.DataGridView3.Name = "DataGridView3";
+            this.DataGridView3.RowHeadersWidth = 62;
+            this.DataGridView3.RowTemplate.Height = 28;
+            this.DataGridView3.Size = new System.Drawing.Size(654, 456);
+            this.DataGridView3.TabIndex = 26;
             // 
             // Column1
             // 
@@ -136,15 +136,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(731, 679);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DataGridView3);
+            this.Controls.Add(this.Lblpaymentcoach);
+            this.Controls.Add(this.Btn_hmpg3);
+            this.Controls.Add(this.Btn_exit3);
             this.Controls.Add(this.btn_hmpage001);
             this.Name = "View_Payment_Coach";
             this.Text = "View_Payment_Coach";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,10 +154,10 @@
         #endregion
 
         private System.Windows.Forms.Button btn_hmpage001;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Btn_hmpg3;
+        private System.Windows.Forms.Button Btn_exit3;
+        private System.Windows.Forms.Label Lblpaymentcoach;
+        private System.Windows.Forms.DataGridView DataGridView3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
