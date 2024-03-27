d2213e7d6d5bdaf2ff9f47dca1b33d1618c0c11c
jeff liew <liewjeff14@gmail.com>
2024-03-27 13:22:56 +0800

    added recommendations table

--- IOOP Assignment/Add Training Schedule.Designer.cs
+++ IOOP Assignment/Add Training Schedule.Designer.cs
@@ -51,10 +51,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(304, 84);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(456, 129);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.Size = new System.Drawing.Size(198, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Manage Training Schedule";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -62,30 +61,27 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(378, 97);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(567, 149);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 1;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(211, 191);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(316, 294);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(72, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "CoachID";
             // 
             // TrainingID
             // 
             this.TrainingID.AutoSize = true;
-            this.TrainingID.Location = new System.Drawing.Point(211, 146);
-            this.TrainingID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.TrainingID.Location = new System.Drawing.Point(316, 225);
             this.TrainingID.Name = "TrainingID";
-            this.TrainingID.Size = new System.Drawing.Size(56, 13);
+            this.TrainingID.Size = new System.Drawing.Size(82, 20);
             this.TrainingID.TabIndex = 3;
             this.TrainingID.Text = "TrainingID";
             this.TrainingID.Click += new System.EventHandler(this.label4_Click);
@@ -93,97 +89,86 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(214, 226);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(321, 348);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(51, 20);
             this.label4.TabIndex = 4;
             this.label4.Text = "Name";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(214, 257);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(321, 395);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.Size = new System.Drawing.Size(89, 20);
             this.label5.TabIndex = 5;
             this.label5.Text = "Description";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(214, 285);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(321, 438);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.Size = new System.Drawing.Size(46, 20);
             this.label6.TabIndex = 6;
             this.label6.Text = "Level";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(214, 330);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(321, 508);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.Size = new System.Drawing.Size(76, 20);
             this.label8.TabIndex = 8;
             this.label8.Text = "Schedule";
             // 
             // txtTrainingID
             // 
-            this.txtTrainingID.Location = new System.Drawing.Point(297, 142);
-            this.txtTrainingID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTrainingID.Location = new System.Drawing.Point(446, 218);
             this.txtTrainingID.Name = "txtTrainingID";
-            this.txtTrainingID.Size = new System.Drawing.Size(173, 20);
+            this.txtTrainingID.Size = new System.Drawing.Size(258, 26);
             this.txtTrainingID.TabIndex = 9;
             // 
             // txtCoachID
             // 
-            this.txtCoachID.Location = new System.Drawing.Point(297, 191);
-            this.txtCoachID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCoachID.Location = new System.Drawing.Point(446, 294);
             this.txtCoachID.Name = "txtCoachID";
-            this.txtCoachID.Size = new System.Drawing.Size(173, 20);
+            this.txtCoachID.Size = new System.Drawing.Size(258, 26);
             this.txtCoachID.TabIndex = 10;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(297, 226);
-            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Location = new System.Drawing.Point(446, 348);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(173, 20);
+            this.txtName.Size = new System.Drawing.Size(258, 26);
             this.txtName.TabIndex = 11;
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(297, 257);
-            this.txtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDescription.Location = new System.Drawing.Point(446, 395);
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(173, 20);
+            this.txtDescription.Size = new System.Drawing.Size(258, 26);
             this.txtDescription.TabIndex = 12;
             // 
             // txtLevel
             // 
-            this.txtLevel.Location = new System.Drawing.Point(297, 285);
-            this.txtLevel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLevel.Location = new System.Drawing.Point(446, 438);
             this.txtLevel.Name = "txtLevel";
-            this.txtLevel.Size = new System.Drawing.Size(173, 20);
+            this.txtLevel.Size = new System.Drawing.Size(258, 26);
             this.txtLevel.TabIndex = 13;
             // 
             // txtSchedule
             // 
-            this.txtSchedule.Location = new System.Drawing.Point(297, 323);
-            this.txtSchedule.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSchedule.Location = new System.Drawing.Point(446, 497);
             this.txtSchedule.Name = "txtSchedule";
-            this.txtSchedule.Size = new System.Drawing.Size(173, 20);
+            this.txtSchedule.Size = new System.Drawing.Size(258, 26);
             this.txtSchedule.TabIndex = 15;
             // 
             // btnAddTrainingSchedule
             // 
-            this.btnAddTrainingSchedule.Location = new System.Drawing.Point(155, 406);
-            this.btnAddTrainingSchedule.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddTrainingSchedule.Location = new System.Drawing.Point(232, 625);
             this.btnAddTrainingSchedule.Name = "btnAddTrainingSchedule";
-            this.btnAddTrainingSchedule.Size = new System.Drawing.Size(127, 31);
+            this.btnAddTrainingSchedule.Size = new System.Drawing.Size(190, 48);
             this.btnAddTrainingSchedule.TabIndex = 16;
             this.btnAddTrainingSchedule.Text = "Add";
             this.btnAddTrainingSchedule.UseVisualStyleBackColor = true;
@@ -191,10 +176,9 @@
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(323, 406);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEdit.Location = new System.Drawing.Point(484, 625);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(113, 31);
+            this.btnEdit.Size = new System.Drawing.Size(170, 48);
             this.btnEdit.TabIndex = 17;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -202,10 +186,9 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(471, 406);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Location = new System.Drawing.Point(706, 625);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(113, 31);
+            this.btnDelete.Size = new System.Drawing.Size(170, 48);
             this.btnDelete.TabIndex = 18;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -213,10 +196,9 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(324, 460);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Location = new System.Drawing.Point(486, 708);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(113, 31);
+            this.btnExit.Size = new System.Drawing.Size(170, 48);
             this.btnExit.TabIndex = 19;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -224,10 +206,10 @@
             // 
             // Add_Training_Schedule
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(758, 521);
+            this.ClientSize = new System.Drawing.Size(1137, 802);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
@@ -246,9 +228,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Add_Training_Schedule";
-            this.Text = "Add_Edit_Delete_Training_Schedule";
+            this.Text = "Coach_Add_Edit_Delete_Training_Schedule";
             this.Load += new System.EventHandler(this.Add_Training_Schedule_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
